482382a0ee4505b30a99dc7bc354c623f4af0936
yongxinhsu <74400650+yongxinhsu@users.noreply.github.com>
2020-11-13 16:02:06 +0800

    Add files via upload

---
+++ myBlocks/blockly/blocks.js
@@ -0,0 +1,10 @@
+Blockly.Blocks['block_type'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("打招呼");
+    this.setOutput(true, null);
+    this.setColour(200);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
---
+++ myBlocks/blockly/javascript.js
@@ -0,0 +1,6 @@
+Blockly.JavaScript['block_type'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'alert("我是誰?")';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
